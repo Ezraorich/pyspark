9a24f79b2ad8602726ffaeffad2846941a6c24b6
Saltanat Khalyk <43410356+Ezraorich@users.noreply.github.com>
2022-08-08 18:53:15 +0600

    Update df_select.py

--- df_select.py
+++ df_select.py
@@ -2,3 +2,6 @@ NNN.filter(col('primarySpecialtyCode')!=('390200000X')).display()
 
 
 physician.alias('l').join(education.alias('r'), on='physicianId').display()
+
+# selecting not NUll and only distinct values from 2 columns.
+display(CList.select('Specialty', 'Idc').distinct().where(col("Specialty").isNotNull()).limit(100))
