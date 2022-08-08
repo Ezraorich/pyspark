0e227f4fa7eff8adf8a2b993391d56772c071fa7
Saltanat Khalyk <43410356+Ezraorich@users.noreply.github.com>
2022-08-08 19:03:38 +0600

    Update df_select.py

--- df_select.py
+++ df_select.py
@@ -5,3 +5,5 @@ physician.alias('l').join(education.alias('r'), on='physicianId').display()
 
 # selecting not NUll and only distinct values from 2 columns.
 display(CList.select('Specialty', 'Idc').distinct().where(col("Specialty").isNotNull()).limit(100))
+ss = CList.select('Specialty', 'Idc').distinct().where(col("Specialty").isNotNull())
+ss.dropDuplicates(['primarySpecialty']).display()
