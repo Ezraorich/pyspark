4060166707e467796eeeb930c93d1e01772d85ea
Saltanat Khalyk <43410356+Ezraorich@users.noreply.github.com>
2022-08-15 19:46:06 +0600

    Update repartition.py

--- repartition.py
+++ repartition.py
@@ -4,3 +4,11 @@ A.count()
 B = df4.filter(df4.ArticleDate_Year.isNotNull()).select('ArticleDate_Year')
 B = B.repartition(4*4*8).persist()
 B.count()
+
+
+d = df4.select('Pub_Year','PubDate_MedDate', 'Article_Year')
+
+coverage = d.count()-d.filter((col('Pub_Year').isNull())&(col('PubDate_MedDate').isNull())& (col('Article_Year').isNull()) ).count() 
+print('Pub_Year, Article_Year and PubDate_MedDate filled amount: ',coverage)
+print('Overall number:',d.count())
+print('Percentage of coverage with Pub_Year, Article_Year and PubDate_MedDate:',(coverage/d.count())*100)
