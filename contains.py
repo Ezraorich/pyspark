8419df6589983b1c902abdf54319d6a6e6fbc4d4
Saltanat Khalyk <43410356+Ezraorich@users.noreply.github.com>
2022-08-16 14:06:47 +0600

    Update contains.py

--- contains.py
+++ contains.py
@@ -6,4 +6,24 @@ df.filter(col("name").contains("mes")).show()
 from pyspark.sql.functions import col
 df.filter(col("name").contains("mes")).show()
 
-df.filter(col("name").like("%mes%")).show()
+df.filter(col("name").like("%mes%")).show()from pyspark.sql import functions as f
+
+
+
+from pyspark.sql import functions as f
+
+df3 = hospital.alias('a').join(pl.alias('b'), f.col('b.practiceLocationAdditionalName').contains(f.col('a.hospitalName')) & (f.col('a.hospitalName') != f.col('b.practiceLocationAdditionalName')), 'leftanti')
+df3.show(truncate=False)
+
+pl.filter((lower(col("practiceLocationAdditionalName")).contains(lower(col("practiceLocationMainName"))))).dropDuplicates(['N']).count()
+
+
+S= df14.select('formattedDate').withColumn('length', 
+                F.length(col('formattedDate')))
+
+
+CList.alias('l').join(dfPI.alias('r'), on='physician').count()
+
+photo  = photo.select('imageURL', 'N').where(col("imageURL").isNotNull())
+
+ss =CMSList.select('primarySpecialty','primarySubSpecialty','N','firstName','lastName').distinct().where((col("primarySpecialty").isNotNull())&col("primarySubSpecialty").isNotNull())
