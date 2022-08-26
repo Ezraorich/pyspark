774258ab3d0f7adc245f73ef51fb2e6f05a15bbf
Saltanat Khalyk <43410356+Ezraorich@users.noreply.github.com>
2022-08-26 20:42:46 +0600

    Create dbutils_widgets.py

---
+++ dbutils_widgets.py
@@ -0,0 +1,12 @@
+from pyspark.sql.functions import lit
+
+
+dbutils.widgets.text('p_data_source', '')
+v_data_source = bdutils.widgets.get('p_data_source')
+
+circuits_renamed_df = circuits_selected_df.withColumnrenamed('circuitsId', 'circuit_id')\
+.withColumnRenamed('circuitref', circuit_ref)\
+.withColumnRenamed('lat', 'latitude')\
+.withColumnRenamed('lng', 'longitude')\
+.withColumnRenamed('alt', 'altitude')\
+.withColumn('data_source', lit(v_data_source))
