ec8934857ad4c8138c61417d0a3361765f7f9415
Saltanat Khalyk <43410356+Ezraorich@users.noreply.github.com>
2022-08-27 17:46:40 +0600

    Create rank.py

---
+++ rank.py
@@ -0,0 +1,8 @@
+from pyspark.sql.window import Window
+from pyspark.sql.functions import rank, col
+
+n = 3
+dd.select(col('SpecialtyName'), row_number().over(window).alias('row_number')) \
+  .where(col('row_number') <= n) \
+  .filter()
+  .display()
