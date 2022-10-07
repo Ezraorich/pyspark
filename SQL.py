f61672b5063ea9dd0551e058377135187bf3544b
Saltanat Khalyk <43410356+Ezraorich@users.noreply.github.com>
2022-10-07 14:55:46 +0600

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -7,3 +7,16 @@ select team_name,
   group by team name
   HAVING COUNT(1) >= 100
   ORDER BY avg_points DESC
+
+  
+  
+  select team_name,
+  COUNT(1) as total_races, 
+  SUM(calculted_points) AS total_points,
+  AVG(calculated_points) AS avg_points
+  RANK() OVER (ORDER BY AVG(calculated_points) DESC) driver_rank
+  from f1_presentation.calculated_race_results
+  where race_year between 2001 and 2011
+  group by team name
+  HAVING COUNT(1) >= 100
+  ORDER BY avg_points DESC
