3678624bfa4c7be48c07cafe2bcb9df0bafee521
Saltanat Khalyk <43410356+Ezraorich@users.noreply.github.com>
2022-10-17 18:50:05 +0600

    Update withColumnRenamed.py

--- withColumnRenamed.py
+++ withColumnRenamed.py
@@ -4,7 +4,7 @@ from pyspark.sql.functions import lit
 dbutils.widgets.text('p_data_source', '')
 v_data_source = bdutils.widgets.get('p_data_source')
 
-circuits_renamed_df = circuits_selected_df.withColumnrenamed('circuitsId', 'circuit_id')\
+circuits_renamed_df = circuits_selected_df.withColumnRenamed('circuitsId', 'circuit_id')\
 .withColumnRenamed('circuitref', circuit_ref)\
 .withColumnRenamed('lat', 'latitude')\
 .withColumnRenamed('lng', 'longitude')\
