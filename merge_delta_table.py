e5fbec4f14ae43c72f788036f9b9f8c1cfd9d5df
Saltanat Khalyk <43410356+Ezraorich@users.noreply.github.com>
2022-11-11 14:45:19 +0600

    Update merge_delta_table.py

--- merge_delta_table.py
+++ merge_delta_table.py
@@ -1,9 +1,11 @@
+spark.conf.set('spark.databricks.optimizer.dynamicPartitionPruning', 'true')
+
 from delta.tables import DeltaTable
 if (spark._jsparkSession.catalog().tableExists('f1_processes.results')):
   deltaTable =DeltaTable.forPath(spark, '/mnt/formula1d/processes/results')
   deltaTable.alias('tgt').merge(
     results_final_df.alias('src'),
-    'tgt.results_id = src.result_id') \
+    'tgt.results_id = src.result_id AND tgt.race_id = src.race_id') \
   .whenMatchedUpdateAll()\
   .whenNotMatchedInsertAll()\
   .execute()
