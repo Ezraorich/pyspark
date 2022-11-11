ebb92526c0899aa9cefcde02478bb112e85b7b26
Saltanat Khalyk <43410356+Ezraorich@users.noreply.github.com>
2022-11-11 14:55:52 +0600

    Update merge_delta_table.py

--- merge_delta_table.py
+++ merge_delta_table.py
@@ -11,3 +11,23 @@ if (spark._jsparkSession.catalog().tableExists('f1_processes.results')):
   .execute()
 else:
   results_final_df.write.moode('overwrite').partitionBy('race_id').format('delta").saveAsTable("f1_processed.results")
+                                                                          
+                                                                          
+### function:
+merge_condition = 'tgt.results_id = src.result_id AND tgt.race_id = src.race_id'
+                                                                          
+def merge_delta_table (input_df, db_name, table_name, forlder_path, merge_condition, partition_column):
+  spark.conf.set('spark.databricks.optimizer.dynamicPartitionPruning', 'true')
+
+  from delta.tables import DeltaTable
+  if (spark._jsparkSession.catalog().tableExists(f'{db_name}.{table_name}')):
+    deltaTable =DeltaTable.forPath(spark, f'{folder_path}/{table_name}')
+    deltaTable.alias('tgt').merge(
+      input_df.alias('src'),
+      merge_condition) \
+    .whenMatchedUpdateAll()\
+    .whenNotMatchedInsertAll()\
+    .execute()
+  else:
+    input_df.write.moode('overwrite').partitionBy(partition_column).format('delta").saveAsTable(f"{db_name}.{table_name}")                                                                         
+                                                                          
