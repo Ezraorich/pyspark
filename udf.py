2262e8866f63da4fa5c5654224f3fd70e50de89c
Saltanat Khalyk <43410356+Ezraorich@users.noreply.github.com>
2023-01-20 19:47:21 +0600

    Update udf.py

--- udf.py
+++ udf.py
@@ -17,7 +17,28 @@ def getBrandName(str1):
 
     return data
         
-        
+      
     
-    #data = [char for char in my_list if char.islower()]
-    #return ' '.join(data)
+    
+@udf(returnType=StringType()) 
+def getStrength(str1):
+    str1 = re.sub("\[.*?\]","", str1)
+    str1 = str1.replace('. ', '')
+    my_list = str1.split(' ')
+    data = []
+    list_length = len(my_list)
+    for i in range(len(my_list)):
+        a = re.sub('[^\d.,]' , '', my_list[i])
+        if (a!='') and (i!=list_length):
+            
+            data.append(my_list[i])
+            data.append(my_list[i+1]) 
+            if len(data)>2:
+                x = len(data)
+                strength = data[x-2:x+1]
+            else:
+                strength = data
+            strength_with_MG = ' '.join(strength)
+            if len(data) == 0:
+                strength_with_MG = None            
+    return strength_with_MG
